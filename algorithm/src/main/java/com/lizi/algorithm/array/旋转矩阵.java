c5f82d61a6354175833db84e4cb4151e6d49ee56
dyed-bear <1839833172@qq.com>
2025-08-19 08:08:04 +0800

    【2025-08-19 08:07:58】: 二维数组-旋转矩阵

---
+++ algorithm/src/main/java/com/lizi/algorithm/array/旋转矩阵.java
@@ -0,0 +1,141 @@
+package com.lizi.algorithm.array;
+
+/**
+ * ClassName: 旋转矩阵
+ * Description:
+ *
+ * @Author 染染熊
+ * @Version 1.0
+ * @Create 2025/8/17 16:46
+ * 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+ * <p>
+ * 不占用额外内存空间能否做到？
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 给定 matrix =
+ * [
+ * [1,2,3],
+ * [4,5,6],
+ * [7,8,9]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [7,4,1],
+ * [8,5,2],
+ * [9,6,3]
+ * ]
+ * 示例 2：
+ * <p>
+ * 给定 matrix =
+ * [
+ * [ 5, 1, 9,11],
+ * [ 2, 4, 8,10],
+ * [13, 3, 6, 7]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [15,13, 2, 5],
+ * [14, 3, 4, 1],
+ * [12, 6, 8, 9],
+ * [16, 7,10,11]
+ * ]
+ * <p>
+ * 作者：LeetCode
+ * 链接：https://leetcode.cn/leetbook/read/array-and-string/clpgd/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class 旋转矩阵 {
+
+    /**
+     * 解法1：使用中间数组转换
+     *
+     * @param matrix
+     */
+    public static void rotate1(int[][] matrix) {
+        int n = matrix.length;
+        int[][] matrix_new = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
+    }
+
+
+    /**
+     * 解法2：原地旋转法
+     * 详细讲解详见力扣：https://leetcode.cn/problems/rotate-matrix-lcci/
+     *
+     * @param matrix
+     */
+    public static void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < (n + 1) / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+
+
+
+    /**
+     * 解法3：使用翻转法-先水平翻转再对角线翻转
+     * @param matrix
+     */
+    public static void rotate3(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    /**
+     * 测试方法
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate3(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
+}
+
