ea289e25d68201f0109cc327d88ee9f58a465fe4
dyed-bear <1839833172@qq.com>
2025-08-21 07:59:34 +0800

    init1

---
+++ algorithm/src/main/java/com/lizi/algorithm/array/对角线遍历.java
@@ -0,0 +1,45 @@
+package com.lizi.algorithm.array;
+
+/**
+ * ClassName: 对角线遍历
+ * Description:
+ *
+ * @Author 染染熊
+ * @Version 1.0
+ * @Create 2025/8/21 7:51
+对角线遍历
+给你一个大小为 m x n 的矩阵 mat ，请以对角线遍历的顺序，用一个数组返回这个矩阵中的所有元素。
+
+ 
+
+示例 1：
+
+
+输入：mat = [[1,2,3],[4,5,6],[7,8,9]]
+输出：[1,2,4,7,5,3,6,8,9]
+示例 2：
+
+输入：mat = [[1,2],[3,4]]
+输出：[1,2,3,4]
+ 
+
+提示：
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 104
+1 <= m * n <= 104
+-105 <= mat[i][j] <= 105
+相关标签
+
+Java
+
+
+
+作者：LeetCode
+链接：https://leetcode.cn/leetbook/read/array-and-string/cuxq3/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class 对角线遍历 {
+}
